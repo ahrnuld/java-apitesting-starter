14a981bc13d6069b35546fdae38e9da34659d67b
Wim Wiltenburg <wim.wiltenburg@gmail.com>
2023-05-06 17:30:59 +0200

    Add given to scenario

--- src/test/java/nl/inholland/apidemo/cucumber/steps/BaseStepDefinitions.java
+++ src/test/java/nl/inholland/apidemo/cucumber/steps/BaseStepDefinitions.java
@@ -6,7 +6,4 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.DEFINED_PORT)
 @CucumberContextConfiguration
 public class BaseStepDefinitions {
-
-
-
 }
--- src/test/java/nl/inholland/apidemo/cucumber/steps/CarStepDefinitions.java
+++ src/test/java/nl/inholland/apidemo/cucumber/steps/CarStepDefinitions.java
@@ -1,6 +1,7 @@
 package nl.inholland.apidemo.cucumber.steps;
 
 import com.jayway.jsonpath.JsonPath;
+import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
 import org.junit.jupiter.api.Assertions;
@@ -11,12 +12,26 @@ import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpMethod;
 import org.springframework.http.ResponseEntity;
 
+import java.util.Arrays;
+import java.util.List;
+
 public class CarStepDefinitions extends BaseStepDefinitions {
 
     @Autowired
     private TestRestTemplate restTemplate;
 
     private ResponseEntity<String> response;
+
+    @Given("The endpoint for {string} is available for method {string}")
+    public void theEndpointForIsAvailable(String endpoint, String method) {
+        response = restTemplate.exchange("/" + endpoint, HttpMethod.OPTIONS, new HttpEntity<>(null, new HttpHeaders()), String.class);
+        List<String> options = Arrays.stream(response.getHeaders()
+                        .get("Allow")
+                        .get(0)
+                        .split(","))
+                .toList();
+        Assertions.assertTrue(options.contains(method.toUpperCase()));
+    }
 
     @When("^I retrieve all cars$")
     public void i_retrieve_all_cars() {
