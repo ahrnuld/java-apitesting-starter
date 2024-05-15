20cfde8fcea1ab33ffb8343f232a251649206e94
Mark de Haan <mark@starwave.nl>
2024-05-15 21:37:27 +0200

    Cucumber starting project prepared

--- src/test/java/nl/inholland/apidemo/cucumber/steps/BaseStepDefinitions.java
+++
@@ -1,9 +0,0 @@
-package nl.inholland.apidemo.cucumber.steps;
-
-import io.cucumber.spring.CucumberContextConfiguration;
-import org.springframework.boot.test.context.SpringBootTest;
-
-@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.DEFINED_PORT)
-@CucumberContextConfiguration
-public class BaseStepDefinitions {
-}
